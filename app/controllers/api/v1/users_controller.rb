2054bf3b2756c67ebcf693df3cb37c3042e4e625
Pavlo Semehen <semegen2010@gmail.com>
2020-04-06 12:54:41 +0300

    Rename second_name to last_name

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -23,7 +23,7 @@ class Api::V1::UsersController < Api::BaseController
   private
 
   def user_params
-    params.permit(:first_name, :second_name, :address, :phone_number)
+    params.permit(:first_name, :last_name, :address, :phone_number)
   end
 
   def resources
