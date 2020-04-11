87ed51d7272c825c5645882abee892510357ed2b
Pavlo Semehen <semegen2010@gmail.com>
2020-04-11 20:16:52 +0300

    Add role for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
+
+  enum role: %i[super_admin agency_admin agency_employee indvidual_employee consumer]
+
+  after_initialize do
+    self.role ||= :consumer if new_record?
+  end
 end
