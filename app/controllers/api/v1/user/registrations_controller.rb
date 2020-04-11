97483e458b51a3aa30d528a6bd1d9677e53da277
Pavlo Semehen <semegen2010@gmail.com>
2020-04-11 20:44:34 +0300

    Add role for users. Validate presense of fields while sign-up

--- app/controllers/api/v1/user/registrations_controller.rb
+++ app/controllers/api/v1/user/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::User::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   respond_to :json
@@ -43,9 +43,9 @@ class Api::V1::User::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name role address phone_number])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
 
-  enum role: %i[super_admin agency_admin agency_employee indvidual_employee consumer]
+  enum role: %i[super_admin agency_admin agency_employee individual_employee consumer]
 
-  after_initialize do
-    self.role ||= :consumer if new_record?
-  end
+  validates :first_name, :last_name, :role, presence: true
+  validates :phone_number, :address, presence: true, if: -> { agency_admin? || individual_employee? }
 end
