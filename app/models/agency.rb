12a967d4f61edef9a1fedc44681e9d24f06160d2
Pavlo Semehen <semegen2010@gmail.com>
2020-04-15 23:04:03 +0300

    Add Schedule as polymorphic

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,6 +1,7 @@
 class Agency < ApplicationRecord
   has_many :employee_informations, dependent: :restrict_with_error
   has_many :employees, through: :employee_informations
+  has_one :schedule, as: :schedulable
 
   validates :name, presence: true
 end
