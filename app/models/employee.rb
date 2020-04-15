60f61eea2bd0e5c8feaa562de395d270b435077e
Pavlo Semehen <semegen2010@gmail.com>
2020-04-16 01:43:16 +0300

    Add Shedule and Service entities

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,8 @@ class Employee < User
   has_one :employee_information, dependent: :destroy
   has_one :agency, through: :employee_information
   has_one :schedule, as: :schedulable
+  #has_many :user_services, foreign_key: :user_id
+  #has_many :services, through: :user_services
 
   accepts_nested_attributes_for :employee_information
 end
