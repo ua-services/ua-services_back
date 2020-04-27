39b995563023018f625f5a37897598797498fd5c
Pavlo Semehen <semegen2010@gmail.com>
2020-04-27 22:03:23 +0300

    Change validations, make agency required for agency admin

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,9 +1,9 @@
 class Agency < ApplicationRecord
   has_many :employee_informations, dependent: :restrict_with_error
   has_many :employees, through: :employee_informations
-  belongs_to :owner, class_name: 'Employee', foreign_key: 'owner_id', optional: true, inverse_of: :own_agency
+  belongs_to :owner, class_name: 'Employee', foreign_key: 'owner_id', inverse_of: :own_agency
 
-  validates :name, presence: true
+  validates :name, :phone_number, presence: true
   # TODO: validate email with regex
   validates :name, :email, uniqueness: true
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,22 +1,24 @@
 class Employee < User
+  has_one :employee_information, dependent: :destroy
+  has_one :agency, through: :employee_information
+  has_one :own_agency, class_name: 'Agency', foreign_key: 'owner_id', inverse_of: :owner, dependent: :destroy
   has_many :employee_services
   has_many :services, through: :employee_services
   has_many :bookings, dependent: :restrict_with_error
-  has_one :own_agency, class_name: 'Agency', foreign_key: 'owner_id', inverse_of: :owner, dependent: :destroy
-  has_one :employee_information, dependent: :destroy
-  has_one :agency, through: :employee_information
 
-  # TODO: move to mutations
-  after_create_commit :set_agency_id_on_employee_informations, on: :create, if: proc { |employee| employee.agency_admin? }
+  after_create_commit :set_agency_if_agency_admin, on: :create, if: proc { |employee| employee.agency_admin? }
 
   accepts_nested_attributes_for :employee_information
   accepts_nested_attributes_for :own_agency
 
   delegate :agency_id, to: :employee_information, allow_nil: true
 
+  validates :address, presence: true
+
   private
 
-  def set_agency_id_on_employee_informations
+  def set_agency_if_agency_admin
+    byebug
     self.agency = own_agency
   end
 end
