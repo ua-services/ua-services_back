c13bd5037991933d47b0c0bc692e54c8757beaa6
Pavlo Semehen <semegen2010@gmail.com>
2020-04-27 12:04:26 +0300

    Update seeds, move callback from agency to employee model

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -3,15 +3,7 @@ class Agency < ApplicationRecord
   has_many :employees, through: :employee_informations
   belongs_to :owner, class_name: 'Employee', foreign_key: 'owner_id', optional: true, inverse_of: :own_agency
 
-  after_create_commit :set_agency_id_on_employee_informations
-
   validates :name, presence: true
-  # Todo validate email with regex
+  # TODO: validate email with regex
   validates :name, :email, uniqueness: true
-
-  private
-
-  def set_agency_id_on_employee_informations
-    owner.agency = self
-  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,8 +6,17 @@ class Employee < User
   has_one :employee_information, dependent: :destroy
   has_one :agency, through: :employee_information
 
+  # TODO: move to mutations
+  after_create_commit :set_agency_id_on_employee_informations, on: :create, if: proc { |employee| employee.agency_admin? }
+
   accepts_nested_attributes_for :employee_information
   accepts_nested_attributes_for :own_agency
 
   delegate :agency_id, to: :employee_information, allow_nil: true
+
+  private
+
+  def set_agency_id_on_employee_informations
+    self.agency = own_agency
+  end
 end
