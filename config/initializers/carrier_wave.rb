b6068fd7e033ab337a887ceda586503fece36c0c
Pavlo Semehen <semegen2010@gmail.com>
2020-05-25 14:45:14 +0300

    updates saving g-cloud creds in env variables

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: "Google",
-    google_project: "ua-services",
-    google_json_key_location: Rails.root.join("config/secrets/ua-services-278219-94d3f3a48d94.json")
+    google_project: RCreds.fetch(:google_cloud_storage_project_name),
+    google_json_key_string: RCreds.fetch(:google_cloud_storage_credentials)
   }
-  config.fog_directory  = 'ua-services'
+  config.fog_directory  = RCreds.fetch(:google_cloud_storage_bucket_name)
   config.fog_public     = true
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
 end
